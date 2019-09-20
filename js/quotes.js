66aa9c616040cb96918c40336147f3b531604422
brady simmons <atlbrady30052@gmail.com>
2019-09-19 22:03:43 -0400

    Updated-File

---
+++ js/quotes.js
@@ -0,0 +1,32 @@
+var quotes = [
+    {
+      quote: "I can accept failure, everyone fails at something. But I cannot accept not trying.",
+      source: "Michael Jordan",
+      citation: "",
+      year: "1998"
+    },
+    {
+      quote: "Hard work beats talent when talent fails to work hard.",
+      source: "Kevin Durant",
+      citation: "",
+      year: "2016"
+    },
+    {
+      quote: "Nothing is given. Everything is earned.",
+      source: "Lebron James",
+      citation: "",
+      year: "2014"
+    },
+    {
+      quote: "Me shooting 40% at the foul line is just God\'s way of saying nobody\’s perfect.",
+      source:"Shaq",
+      citation: "New York Times",
+      year: "2018"
+    },
+    {
+      quote: "I\'m chasing perfection.",
+      source: "Kobe Bryant",
+      citation: "",
+      year: ""
+    },
+];
